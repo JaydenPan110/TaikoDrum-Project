865314cf601e42bb7258541cf9387c0cd87f058f
Geofoods <curious.george71511@gmail.com>
2026-06-21 13:29:20 -0400

    push

--- slotmachine/slotmachine.ino
+++ slotmachine/slotmachine.ino
@@ -1,7 +1,3 @@
-// -------------------------------------------------
-// Copyright (c) 2022 HiBit <https://www.hibit.dev>
-// -------------------------------------------------
-
 #include <pitches.h>
 #include <Servo.h>
 
@@ -9,8 +5,14 @@
 #define BUZZER_PIN 10
 #define BUTTON_PIN 2
 
+#define LED_R 6
+#define LED_G 5
+#define LED_B 3
+
 
 const int servoPin = 9;
+
+char mystr[6] = "Hello";
 
 Servo coinServo;
 
@@ -191,14 +193,26 @@ bool waitingForNote = false;
 
 // Non-blocking state for dispenser
 int coinsToDispense = 0;
-int dispenseStep = 0; // 0=idle, 1=open, 2=wait_fall, 3=close, 4=wait_next
+int dispenseStep = 0; // 0=idle, 1=open_step, 2=wait_fall, 3=close_step, 4=wait_next
 unsigned long dispenseTimer = 0;
+int servoCurrentAngle = 0;
+#define SERVO_STEP_DELAY 30
+#define SERVO_ANGLE_STEP 3
 
-#define LDR_THRESHOLD 3
+#define LDR_THRESHOLD 5
+#define JOY_THRESHOLD 50
 
 // Non-blocking state for LDR
 unsigned long ldrTimer = 0;
 int prevLdrValue = 0;
+unsigned long ldrDebounceTimer = 0;
+bool ldrCooldown = false;
+bool coinDropped = false;
+#define LDR_COOLDOWN_MS 200
+
+// Non-blocking state for joystick
+unsigned long joyTimer = 0;
+int prevJoyY = 512;
 
 // Button state
 int lastButtonState = HIGH;
@@ -206,24 +220,131 @@ int buttonState = HIGH;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 
+// Rainbow LED state
+unsigned long rainbowTimer = 0;
+unsigned int hue = 0;
+
+// Green breathing animation state
+bool rainbowMode = true;
+unsigned long greenFadeTimer = 0;
+int greenFadeLevel = 0;
+bool greenFadeRising = true;
+unsigned long coinDropLockTimer = 0;
+int breathR = 255;
+int breathG = 0;
+int breathB = 255;
+bool spinning = false;
+unsigned long spinEndTime = 0;
+
+void setColor(int r, int g, int b)
+{
+  analogWrite(LED_R, 255 - r);
+  analogWrite(LED_G, 255 - g);
+  analogWrite(LED_B, 255 - b);
+}
+
+void hsvToRgb(unsigned int h, byte &r, byte &g, byte &b)
+{
+  unsigned int sector = h / 60;
+  unsigned int remainder = h % 60;
+  unsigned int ramp = (remainder * 255) / 60;
+
+  byte low = 0;
+  byte high = 255;
+
+  switch (sector) {
+    case 0: r = high; g = ramp; b = low; break;
+    case 1: r = high - ramp; g = high; b = low; break;
+    case 2: r = low; g = high; b = ramp; break;
+    case 3: r = low; g = high - ramp; b = high; break;
+    case 4: r = ramp; g = low; b = high; break;
+    case 5: r = high; g = low; b = high - ramp; break;
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
 
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
 
   coinServo.attach(servoPin);
   coinServo.write(0);
+  servoCurrentAngle = 0;
 
   coinsToDispense = 3;
   dispenseStep = 1;
+  dispenseTimer = millis();
 }
 
 void loop()
 {
   int size = sizeof(durations) / sizeof(int);
+
+  // --- LED (non-blocking) ---
+  if (spinning) {
+    if (millis() >= spinEndTime) {
+      spinning = false;
+      rainbowMode = true;
+      rainbowTimer = millis();
+    } else {
+      if (millis() - greenFadeTimer >= 20) {
+        greenFadeTimer = millis();
+        if (greenFadeRising) {
+          greenFadeLevel += 5;
+          if (greenFadeLevel >= 255) {
+            greenFadeLevel = 255;
+            greenFadeRising = false;
+          }
+        } else {
+          greenFadeLevel -= 5;
+          if (greenFadeLevel <= 0) {
+            greenFadeLevel = 0;
+            greenFadeRising = true;
+          }
+        }
+        setColor(
+          (breathR * greenFadeLevel) / 255,
+          (breathG * greenFadeLevel) / 255,
+          (breathB * greenFadeLevel) / 255
+        );
+      }
+    }
+  } else if (rainbowMode) {
+    if (millis() - rainbowTimer >= 20) {
+      rainbowTimer = millis();
+      byte r, g, b;
+      hsvToRgb(hue, r, g, b);
+      setColor(r, g, b);
+      hue = (hue + 1) % 360;
+    }
+  } else {
+    if (millis() - greenFadeTimer >= 20) {
+      greenFadeTimer = millis();
+      if (greenFadeRising) {
+        greenFadeLevel += 5;
+        if (greenFadeLevel >= 255) {
+          greenFadeLevel = 255;
+          greenFadeRising = false;
+        }
+      } else {
+        greenFadeLevel -= 5;
+        if (greenFadeLevel <= 0) {
+          greenFadeLevel = 0;
+          greenFadeRising = true;
+        }
+      }
+      setColor(
+        (breathR * greenFadeLevel) / 255,
+        (breathG * greenFadeLevel) / 255,
+        (breathB * greenFadeLevel) / 255
+      );
+    }
+  }
 
   // // --- Buzzer (non-blocking) ---
   // if (!waitingForNote) {
@@ -255,33 +376,107 @@ void loop()
   // --- LDR (non-blocking, every 10ms) ---
   if (millis() - ldrTimer >= 10) {
     ldrTimer = millis();
-    int ldrValue = analogRead(LDR_PIN);
-    int diff = ldrValue - prevLdrValue;
-    if (diff < 0 && abs(diff) > LDR_THRESHOLD) {
-      Serial.println("beep");
-      tone(BUZZER_PIN, 1000, 50);
-      prevLdrValue = ldrValue;
-    } else if (diff > 0 && abs(diff) > LDR_THRESHOLD) {
-      prevLdrValue = ldrValue;
+
+    if (ldrCooldown && millis() - ldrDebounceTimer >= LDR_COOLDOWN_MS) {
+      ldrCooldown = false;
     }
+
+    bool calibrating = (millis() < 10000) || (!rainbowMode && millis() - coinDropLockTimer < 10000);
+
+    if (calibrating) {
+      prevLdrValue = analogRead(LDR_PIN);
+    } else if (!ldrCooldown) {
+      int ldrValue = analogRead(LDR_PIN);
+      int diff = ldrValue - prevLdrValue;
+      if (diff < 0 && abs(diff) > LDR_THRESHOLD) {
+        Serial.println("beep");
+        tone(BUZZER_PIN, 1000, 50);
+        prevLdrValue = ldrValue;
+        ldrCooldown = true;
+        ldrDebounceTimer = millis();
+        coinDropped = true;
+
+        if (rainbowMode) {
+          rainbowMode = false;
+          coinDropLockTimer = millis();
+          greenFadeLevel = 0;
+          greenFadeRising = true;
+          greenFadeTimer = millis();
+        }
+      } else if (diff > 0 && abs(diff) > LDR_THRESHOLD) {
+        prevLdrValue = ldrValue;
+      }
+    }
+  }
+
+  // --- Joystick Y-axis (non-blocking, every 20ms) ---
+  if (millis() - joyTimer >= 20) {
+    joyTimer = millis();
+    int joyY = analogRead(A1);
+    int diff = joyY - prevJoyY;
+    if (diff < 0 && abs(diff) > JOY_THRESHOLD) {
+      if (coinDropped) {
+        Serial.println("spin");
+        Serial.write(mystr, 5);
+        coinDropped = false;
+        spinning = true;
+        spinEndTime = millis() + 5000;
+        breathR = 255;
+        breathG = 0;
+        breathB = 0;
+        greenFadeLevel = 0;
+        greenFadeRising = true;
+        greenFadeTimer = millis();
+      } else {
+        Serial.println("down");
+        if (!rainbowMode) {
+          breathR = 0;
+          breathG = 255;
+          breathB = 255;
+        }
+      }
+    }
+    prevJoyY = joyY;
   }
 
   // --- Dispenser (non-blocking) ---
   if (coinsToDispense > 0) {
     switch (dispenseStep) {
-      case 1:
-        coinServo.write(110);
-        dispenseTimer = millis();
-        dispenseStep = 2;
+      case 1: // Gradually open
+        if (millis() - dispenseTimer >= SERVO_STEP_DELAY) {
+          servoCurrentAngle += SERVO_ANGLE_STEP;
+          if (servoCurrentAngle >= 125) {
+            servoCurrentAngle = 125;
+            coinServo.write(servoCurrentAngle);
+            dispenseTimer = millis();
+            dispenseStep = 2;
+          } else {
+            coinServo.write(servoCurrentAngle);
+            dispenseTimer = millis();
+          }
+        }
         break;
-      case 2:
+      case 2: // Wait at open for coin to fall
         if (millis() - dispenseTimer >= 500) {
-          coinServo.write(0);
           dispenseTimer = millis();
           dispenseStep = 3;
         }
         break;
-      case 3:
+      case 3: // Gradually close
+        if (millis() - dispenseTimer >= SERVO_STEP_DELAY) {
+          servoCurrentAngle -= SERVO_ANGLE_STEP;
+          if (servoCurrentAngle <= 0) {
+            servoCurrentAngle = 0;
+            coinServo.write(servoCurrentAngle);
+            dispenseTimer = millis();
+            dispenseStep = 4;
+          } else {
+            coinServo.write(servoCurrentAngle);
+            dispenseTimer = millis();
+          }
+        }
+        break;
+      case 4: // Wait before next coin
         if (millis() - dispenseTimer >= 500) {
           coinsToDispense--;
           if (coinsToDispense > 0) {
